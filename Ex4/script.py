ae7b5545ba6e6a54ffe181a09f21757259e2af0a
toan <toanleduc1@gmail.com>
2019-11-24 12:15:57 +0700

    Can run all code

--- Ex4/script.py
+++ Ex4/script.py
@@ -4,53 +4,14 @@ import numpy as np
 from scipy.fftpack import fft2
 from scipy.fftpack import ifft2
 
-def scale(X, x_min, x_max):
-    nom = (X-X.min(axis=0))*(x_max-x_min)
-    denom = X.max(axis=0) - X.min(axis=0)
-    denom[denom==0] = 1
-    return x_min + nom/denom 
-
-
 im = plt.imread('origin.png')
+im_size = im.shape[0] + im.shape[1] + im.shape[2]
+fft_im = fft2(im)
+imageio.imwrite('real.tif', fft_im.real)
+imageio.imwrite('imag.tif', fft_im.imag)
 
-rf_im = fft2(im).real
-rf_min = np.min(rf_im)
-rf_im[:,:,0] = scale(rf_im[:,:,0], 0, 255)
-rf_im[:,:,1] = scale(rf_im[:,:,1], 0, 255)
-rf_im[:,:,2] = scale(rf_im[:,:,2], 0, 255)
-try:
-    rf_im[:,:,3] = scale(rf_im[:,:,3], 0, 255)
-except:
-    print("FFT: image don't has alpha field")
-rf_im = rf_im.astype('uint8')
-imageio.imwrite('real.png', rf_im)
-
-if_im = fft2(im).imag
-if_im[:,:,0] = scale(if_im[:,:,0], 0, 255)
-if_im[:,:,1] = scale(if_im[:,:,1], 0, 255)
-if_im[:,:,2] = scale(if_im[:,:,2], 0, 255)
-try:
-    if_im[:,:,3] = scale(if_im[:,:,3], 0, 255)
-except:
-    print("FFT: image don't has alpha field")
-if_im = if_im.astype('uint8')
-imageio.imwrite('imag.png', if_im)
-
-re_im = plt.imread('real.png').astype('complex128')
-re_im.imag = plt.imread('imag.png')
-re_im = ifft2(re_im).real
-re_im[:,:,0] = scale(re_im[:,:,0], 0, 255)
-re_im[:,:,1] = scale(re_im[:,:,1], 0, 255)
-re_im[:,:,2] = scale(re_im[:,:,2], 0, 255)
-try:
-    re_im[:,:,3] = scale(re_im[:,:,3], 0, 255)
-except:
-    print("IFFT: image don't has alpha field")
+re_im = imageio.imread('real.tif').astype('complex128')
+re_im.imag = imageio.imread('imag.tif')
+re_im = 255 * (ifft2(re_im).real - np.min(ifft2(re_im).real))
 re_im = re_im.astype('uint8')
 imageio.imwrite('recover.png', re_im)
-
-test = fft2(im).real.astype('complex128')
-test.imag = fft2(im).imag
-test = 255 * ifft2(test).real
-test = test.astype('uint8')
-imageio.imwrite('test.png', test)
